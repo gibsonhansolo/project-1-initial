90f5b9526777da85badb73446ed371946d2775c1
gibsonhansolo <gib2151201@maricopa.edu>
2025-01-15 19:00:46 -0700

    inital implementation with help of point class

--- src/main/java/com/csc205/project1/Point.java
+++ src/main/java/com/csc205/project1/Point.java
@@ -1,4 +1,66 @@
 package com.csc205.project1;
 
+/**
+ *  Initial implementation of class Point with ChatGPT
+ *  Prompt: In java generate the following code with the given UML picture.
+ */
+
 public class Point {
+    private double x;
+    private double y;
+
+    public Point() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance(Point other) {
+        double dx = this.x - other.x;
+        double dy = this.y - other.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    public double getX() {
+        return this.x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setPoint(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void shiftX(double num) {
+        this.x += num;
+    }
+
+    public void shiftY(double num) {
+        this.y += num;
+    }
+
+    public void rotate(double angle) {
+        this.x = this.x * Math.cos(angle) - this.y * Math.sin(angle);
+        this.y = this.y * Math.sin(angle) + this.y * Math.cos(angle);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + this.x + ", " + this.y + ")";
+    }
 }
